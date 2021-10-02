0856e51e7ba05395405bbdc3ed01f095e909c618
AbdulMoizChishti <moaiz599@gmail.com>
2021-10-03 01:16:15 +0500

    Switch Case

---
+++ src/switchcase/Switchcase.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package switchcase;
+
+/**
+ *
+ * @author Abdul Moiz Chishti
+ */
+public class Switchcase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int day =27;
+        
+        switch(day){
+            
+            case 27:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("tuesday"); 
+                break;
+                
+            default:
+                System.out.println("No such day exsist"); 
+        }
+        
+        
+    }
+    
+}
